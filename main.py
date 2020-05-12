82dc63b468a3031b8ba3674f849752c3a6997699
GrodVidar <vexxart@gmail.com>
2020-05-12 19:11:06 +0200

    should be working

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ from datetime import datetime
 
 ROWS = 4
 COLS = 4
-keys = ['1','2','3','A',
-        '4','5','6','B',
-        '7','8','9','C',
-        '*','0','#','D']
+keys = ['1', '2', '3', 'A',
+        '4', '5', '6', 'B',
+        '7', '8', '9', 'C',
+        '*', '0', '#', 'D']
 
 row_pins = [12, 16, 18, 22]
 col_pins = [19, 15, 13, 11]
-code = ''
+
 with open('key.txt', 'r') as file:
     code = str(file.readline().rstrip())
     print(code)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7,GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 servo1 = GPIO.PWM(7, 50)
 servo1.start(3)
 sleep(0.2)
+
 
 def get_cpu_temp():
     tmp = open('/sys/class/thermal/thermal_zone0/temp')
     cpu = tmp.read()
     tmp.close()
-    return '{:.2f}'.format( float(cpu)/1000) + ' C'
+    return '{:.2f}'.format(float(cpu)/1000) + ' C'
+
 
 def get_time_now():
     return datetime.now().strftime('   %H:%M:%S')
@@ -48,70 +50,72 @@ def open_servo():
 
 
 def loop():
-    mcp.output(3,1)
-    lcd.begin(16,2)
+    mcp.output(3, 1)
+    lcd.begin(16, 2)
     keypad = Keypad.Keypad(keys, row_pins, col_pins, ROWS, COLS)
     keypad.setDebounceTime(50)
-    idle = True
-    while idle:
-        lcd.setCursor(0,0)
-        lcd.message('CPU: ' + get_cpu_temp() + '\n')
-        lcd.message(get_time_now())
-        #check if keypad says *
-        key = keypad.getKey()
-        if key != keypad.NULL:
-            print("you pressed key: ", key)
-            if key == '*':
-                idle = False
-            elif key == '#':
-                lock_servo()
-        sleep(1)
-    lcd.clear()
-    active = True
-    while active:
-        lcd.setCursor(0,0)
-        lcd.message("Enter Code:\n")
-        message = ''
-        entering = True
-        while entering:
+    while True:
+        idle = True
+        while idle:
+            lcd.setCursor(0, 0)
+            lcd.message('CPU: ' + get_cpu_temp() + '\n')
+            lcd.message(get_time_now())
+            # check if keypad says *
             key = keypad.getKey()
             if key != keypad.NULL:
+                print("you pressed key: ", key)
                 if key == '*':
-                    entering = False
-                else:
-                    message += key
-                    lcd.setCursor(0,1)
-                    lcd.message(message)
-        # lcd.message(message)
-        if message == code:
-            lcd.clear()
-            lcd.setCursor(0,0)
-            lcd.message("Opening!")
-            open_servo()
-            sleep(4)
-            active = False
-        else:
-            lcd.clear()
-    destroy()
+                    idle = False
+                elif key == '#':
+                    lock_servo()
+            sleep(1)
+        lcd.clear()
+        active = True
+        while active:
+            lcd.setCursor(0, 0)
+            lcd.message("Enter Code:\n")
+            message = ''
+            entering = True
+            while entering:
+                key = keypad.getKey()
+                if key != keypad.NULL:
+                    if key == '*':
+                        entering = False
+                    else:
+                        message += key
+                        lcd.setCursor(0, 1)
+                        lcd.message(message)
+            # lcd.message(message)
+            if message == code:
+                lcd.clear()
+                lcd.setCursor(0, 0)
+                lcd.message("Opening!")
+                open_servo()
+                sleep(4)
+                active = False
+            else:
+                lcd.clear()
+
 
 def destroy():
     servo1.stop()
     lcd.clear()
     GPIO.cleanup()
 
+
 PCF8574_address = 0x27
 PCF8574A_address = 0x3F
 
 try:
     mcp = PCF8574_GPIO(PCF8574_address)
-except:
+except Exception as e:
     try:
         mcp = PCF8574_GPIO(PCF8574A_address)
-    except:
+    except Exception as ex:
         print("I2C address error!")
         exit(1)
 
-lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4, 5, 6, 7], GPIO=mcp)
 
 if __name__ == '__main__':
     try:
--- website.py
+++ website.py
@@ -1,14 +1,22 @@
 import flask
+import sqlite3
 
 app = flask.Flask(__name__)
 
+
+connection = sqlite3.connect('code', check_same_thread=False)
+cursor = connection.cursor()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print(flask.request.form)
     code = flask.request.form.get('code')
     if code:
-        with open('key.txt','w') as file:
-            file.write(str(code))
+        with open('text.txt', 'w') as f:
+            f.write(str(code))
     return flask.render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug = False)
+    app.run(host="0.0.0.0", port=80, debug=False)
